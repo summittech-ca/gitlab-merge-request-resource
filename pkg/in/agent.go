eabd4fffa6e8e13679ce3c5991ba580b1d4a520c
Eric Bellotti <ericb@summit-tech.ca>
2022-09-16 02:47:57 +0000

    Support for ssh_keys in source

---
+++ pkg/in/agent.go
@@ -0,0 +1,58 @@
+package in
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"os/exec"
+)
+
+type AgentRunner interface {
+	Start() error
+	AddKey(key string) error
+}
+
+func NewAgentRunner() AgentRunner {
+	return &AgentRunnerImpl{
+		sockPath: "/tmp/ssh-agent.sock",
+	}
+}
+
+type AgentRunnerImpl struct {
+	sockPath string
+	agent    *exec.Cmd
+}
+
+func (r *AgentRunnerImpl) Start() error {
+	if r.agent != nil {
+		return nil // already running
+	}
+	agent := exec.Command("ssh-agent", "-a", r.sockPath)
+	agent.Stdin = os.Stdin
+	agent.Stderr = os.Stderr
+	err := agent.Run()
+	if err != nil {
+		return err
+	}
+	r.agent = agent
+	os.Setenv("SSH_AUTH_SOCK", r.sockPath)
+	return nil
+}
+
+func (r AgentRunnerImpl) AddKey(key string) error {
+	command := exec.Command("ssh-add", "-")
+	stdin, err := command.StdinPipe()
+	if err != nil {
+		io.WriteString(stdin, key)
+	}
+	command.Stderr = os.Stderr
+	var b bytes.Buffer
+	b.Write([]byte(key))
+	command.Stdin = &b
+	err = command.Run()
+	if err != nil {
+		return err
+	}
+	stdin.Close()
+	return nil
+}
