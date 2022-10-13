8167cfc586706ec64dca01799fd71c77ed98bf55
Eric Bellotti <ericb@summit-tech.ca>
2022-10-13 12:19:01 +0000

    WIP

--- pkg/in/git.go
+++ pkg/in/git.go
@@ -1,6 +1,7 @@
 package in
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 )
@@ -19,6 +20,10 @@ type DefaultRunner struct {
 func (r DefaultRunner) Run(args ...string) error {
 	cmd := "git"
 	command := exec.Command(cmd, args...)
+	fmt.Print(cmd)
+	fmt.Print(" ")
+	fmt.Println(args)
+
 	command.Stdin = os.Stdin
 	command.Stderr = os.Stderr
 	err := command.Run()
